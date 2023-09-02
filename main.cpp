acddd51ba265ba582e03e41a641c5258e1908a3e
Mungdong <yangfb510195171@gmail.com>
2023-09-02 12:56:37 +0800

    c++环境搭建

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "hello C++" << endl;   /* 终端执行编译命令：gcc hello.cpp -lstdc++ -o hello.exe */
+    system("pause");
+    /* code */
+    return 0;
+}
+
+// #include <stdio.h>
+// #include <windows.h>
+// int main()
+// {
+//     printf("Hello 你好，我是博主 宁在春！\n");
+//     printf("很高兴，能够遇到你！ \n");
+//     system("pause");
+//     return 0;
+// }
